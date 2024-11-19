9273521a4faffebf12b882258c946f65204c9dbe
vrhinehart <vrhinehart@olin.edu>
2024-11-19 13:27:35 -0500

    In-class day 1

--- string_rate_func01.m
+++ string_rate_func01.m
@@ -1,32 +1,43 @@
-%INPUTS
-%t: current time
-%V: system state. V = [U;dUdt] where
-% U and dUdt are n x 1 column vectors
-%string_params: a struct containing the system parameters describing the string
-% string_params.n: number of masses
-% string_params.M: total mass attached to the string
-% string_params.Uf_func: function describing motion of end point
-% string_params.dUfdt_func: time derivative of Uf
-% string_params.Tf: %tension in string
-% string_params.L: %length of string
-% string_params.c: %damping coefficient
-% string_params.dx: %horizontal spacing between masses
+%{
+INPUTS
+t: current time
+V: system state. V = [U;dUdt] where
+U and dUdt are n x 1 column vectors
+string_params: a struct containing the system parameters describing the string
+    string_params.n: number of masses
+    string_params.M: total mass attached to the string
+    string_params.Uf_func: function describing motion of end point
+    string_params.dUfdt_func: time derivative of Uf
+    string_params.Tf: %tension in string
+    string_params.L: %length of string
+    string_params.c: %damping coefficient
+    string_params.dx: %horizontal spacing between masses
+%}
+
 function dVdt = string_rate_func01(t,V,string_params)
-n = string_params.n; %number of masses
-M = string_params.M; %total mass attached to the string
-Uf_func = string_params.Uf_func; %function describing motion of end point
-dUfdt_func = string_params.dUfdt_func; %time derivative of Uf
-Tf = string_params.Tf; %tension in string
-L = string_params.L; %length of string
-c = string_params.c; %damping coefficient
-dx = string_params.dx; %horizontal spacing between masses
-%unpack state variable
-U = V(1:n);
-dUdt = V((n+1):(2*n));
-Uf = Uf_func(t);
-dUfdt = dUfdt_func(t);
-%compute acceleration
-d2Udt2 = %your code here (may take several lines)
-%assemble state derivative
-dVdt = [dUdt;d2Udt2];
+    n = string_params.n; %number of masses
+    M = string_params.M; %total mass attached to the string
+    Uf_func = string_params.Uf_func; %function describing motion of end point
+    dUfdt_func = string_params.dUfdt_func; %time derivative of Uf
+    Tf = string_params.Tf; %tension in string
+    L = string_params.L; %length of string
+    c = string_params.c; %damping coefficient
+    dx = string_params.dx; %horizontal spacing between masses
+
+    %unpack state variable
+    U = V(1:n);
+    dUdt = V((n+1):(2*n));
+    Uf = Uf_func(t);
+    dUfdt = dUfdt_func(t);
+
+    %compute acceleration
+    d2Udt2 = zeros(n,1);
+    d2Udt2(1) = n/M * (Tf/dx * (0 - 2*U(1) + U(2)) + c/dx * (0 - 2*dUdt(1) + dUdt(2)));
+    for i = 2:n-1
+        d2Udt2(i) = n/M * (Tf/dx * (U(i-1) - 2*U(i) + U(i+1)) + c/dx * (dUdt(i-1) - 2*dUdt(i) + dUdt(i+1)));
+    end
+    d2Udt2(n) = n/M * (Tf/dx * (U(n-1) - 2*U(n) + Uf_func(t)) + c/dx * (dUdt(n-1) - 2*dUdt(n) + dUfdt_func(t)));
+
+    %assemble state derivative
+    dVdt = [dUdt;d2Udt2];
 end
--- string_simulation_template01.m
+++ string_simulation_template01.m
@@ -1,32 +1,62 @@
 function string_simulation_template01()
-num_masses = %your code here
-total_mass = %your code here
-tension_force = %your code here
-string_length = %your code here
-damping_coeff = %your code here
-dx = string_length/(num_masses+1);
-amplitude_Uf = %your code here
-omega_Uf = %your code here
-%list of x points (including the two endpoints)
-xlist = linspace(0,string_length,num_masses+2);
-Uf_func = @(t_in) amplitude_Uf*cos(omega_Uf*t_in);
-dUfdt_func = @(t_in) -omega_Uf*amplitude_Uf*sin(omega_Uf*t_in);
-%generate the struct
-string_params = struct();
-string_params.n = num_masses;
-string_params.M = total_mass;
-string_params.Uf_func = Uf_func;
-string_params.dUfdt_func = dUfdt_func;
-string_params.Tf = tension_force;
-string_params.L = string_length;
-string_params.c = damping_coeff;
-string_params.dx = dx;
-%initial conditions
-U0 = %your code here
-dUdt0 = %your code here
-V0 = [U0;dUdt0];
-tspan = %your code here
-%run the integration
-% [tlist,Vlist] = your_integrator(my_rate_func,tspan,V0,...);
-%your code to generate an animation of the system
+    num_masses = 20;
+    total_mass = 2;
+    tension_force = 2;
+    string_length = 3;
+    damping_coeff = 0.05;
+
+    dx = string_length/(num_masses+1);
+
+    amplitude_Uf = 0.2;
+    omega_Uf = pi;
+
+    %list of x points (including the two endpoints)
+    xlist = linspace(0,string_length,num_masses+2);
+
+    Uf_func = @(t_in) amplitude_Uf*cos(omega_Uf*t_in);
+    dUfdt_func = @(t_in) -omega_Uf*amplitude_Uf*sin(omega_Uf*t_in);
+
+    %generate the struct
+    string_params = struct();
+    string_params.n = num_masses;
+    string_params.M = total_mass;
+    string_params.Uf_func = Uf_func;
+    string_params.dUfdt_func = dUfdt_func;
+    string_params.Tf = tension_force;
+    string_params.L = string_length;
+    string_params.c = damping_coeff;
+    string_params.dx = dx;
+
+    %initial conditions
+    U0 = zeros(num_masses,1);
+    dUdt0 = zeros(num_masses,1);
+    V0 = [U0;dUdt0];
+    tspan = [0 20];
+
+    %run the integration
+    rate_func_wrapper = @(t,V) string_rate_func01(t,V,string_params);
+    [tlist,Vlist] = ode45(rate_func_wrapper,tspan,V0);
+
+    figure()
+    zoom = 1;
+    axis([0 num_masses+1 -zoom zoom])
+    hold on
+    x_pos = 0:num_masses+1;
+    V_data = [0, Vlist(1,1:num_masses), Uf_func(tlist(1))];
+    string_plot = plot(x_pos, V_data, '-o');
+    frametimes = zeros(length(tlist),1);
+    tic
+    for i = 1:length(tlist)
+        V_data = [0, Vlist(i,1:num_masses), Uf_func(tlist(i))];
+        set(string_plot,'YData',V_data)
+        frametimes(i) = double(toc);
+        pause(tlist(i) - frametimes(i));
+        drawnow
+    end
+
+    close all
+    figure()
+    plot(frametimes - tlist)
+    xlabel('Timestep')
+    ylabel('Frame Lag (s)')
 end
